98d51a807e8a3e9554521266bed33ca74b81787e
solohubivan <solohubivan@gmail.com>
2026-04-29 21:50:43 +0300

    Initial Commit

---
+++ LearningGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearningGit
+//
+//  Created by Ivan Solohub on 29.04.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearningGit/LearningGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitApp.swift
+//  LearningGit
+//
+//  Created by Ivan Solohub on 29.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
